87989e8331f255157fd3698c49cdb348bced6f08
Paul Harnagel <36869145+PaulHarnagel@users.noreply.github.com>
2019-09-24 11:18:47 -0500

    Create multiply.py

---
+++ multiply.py
@@ -0,0 +1,11 @@
+def multiply_by_2(x):
+    """
+    Multiplies x by 2.
+    
+    Parameters: 
+    arg1 (x): number, int or float
+  
+    Returns: 
+    number: multiple of 2 of arg1
+    """
+    return x * 2
